f0b44b2045cdd73314c5b75f0a84b60b22b0b0c2
Gabriel <gabrielsegarra61@gmail.com>
2024-05-29 16:54:26 -0500

    Added MVC structure and model Object class

---
+++ Trabajo en Clase/src/Model/Zapato.java
@@ -0,0 +1,29 @@
+package Model;
+
+
+
+public class Zapato {
+    private int talla;
+    private String color;
+    
+    public Zapato(){
+        
+    }
+    public Zapato(int talla, String color) {
+        this.talla = talla;
+        this.color = color;
+    }
+    public int getTalla() {
+        return talla;
+    }
+    public void setTalla(int talla) {
+        this.talla = talla;
+    }
+    public String getColor() {
+        return color;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
+    
+}
